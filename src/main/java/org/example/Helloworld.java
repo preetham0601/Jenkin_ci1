41a52ff07e9da1288a9c46cffd11b3b22c67b264
shreyas mr <137146958+preetham0601@users.noreply.github.com>
2026-03-16 10:42:34 +0530

    Update Helloworld.java

--- src/main/java/org/example/Helloworld.java
+++ src/main/java/org/example/Helloworld.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Helloworld {
     public static void main(String[]args){
-        System.out.println("Hello from jenkins ci pipeline");
+        System.out.println("hi from jenkins ci pipeline");
     }
 }
